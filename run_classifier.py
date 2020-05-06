1035a99fcecd4cd26941cae3b721951db212223d
Maradonna <marco.jendryczko@gmx.net>
2020-05-06 18:29:20 +0200

    iterate over dir

--- run_classifier.py
+++ run_classifier.py
@@ -4,6 +4,7 @@ import torchvision
 import torchvision.transforms as transforms
 import torchvision.models as models
 import os
+from PIL import Image
 #GENERAL PARAMETERS
 input_size = 224
 
@@ -14,7 +15,7 @@ num_classes = 9
 data_dir = '/content/drive/My Drive/stylegan2-colab/stylegan2/results/00003-generate-images'
 
 #RESULT_DIR
-res_dir = '/content/drive/My Drive/FMGAN-V2/'
+res_dir = '/content/drive/My Drive/FMGAN-V2/pics/'
 
 #CHECK FOR GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -26,9 +27,9 @@ transform = transforms.Compose(
      transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
 
 #DEFINE DATA
-testset = torchvision.datasets.ImageFolder(data_dir, transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=1,
-                                         shuffle=False, num_workers=4)
+#testset = torchvision.datasets.ImageFolder(data_dir, transform)
+#testloader = torch.utils.data.DataLoader(testset, batch_size=1,
+#                                         shuffle=False, num_workers=4)
 
 
 classes = ['African:Caribbean', 'Asian (Central)', 'East Asian', 'Mediterranean:Hispanic', 'Mixed Race (Black:White)', 'North African:Middle Eastern', 'Northern European', 'Pacific Islander', 'South East Asian']
@@ -48,9 +49,12 @@ model.to(device)
 
 #RUNNING
 with torch.no_grad():
-    for data in testloader:
-        inputs, labels = data[0].to(device), data[1].to(device)
-        print(inputs)
+    for j, entry in enumerate(os.scandir(data_dir)):
+        print(entry.path)
+        img = Image.open(entry.path)
+        for trans in transform:
+            img = trans(img)
+        img = img.to(device)
         outputs = model(inputs)
         _, predicted = torch.max(outputs.data, 1)
         print(predicted.cpu().numpy())
