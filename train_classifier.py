df0d66cf3cedb3b34415d0ec200dc6f00a6ecafa
Maradonna <marco.jendryczko@gmx.net>
2020-05-06 14:53:37 +0200

    added imports

--- train_classifier.py
+++ train_classifier.py
@@ -1,10 +1,20 @@
+from __future__ import print_function
+from __future__ import division
 import torch
-import torchvision
-import torchvision.transforms as transforms
-import torchvision.models as models
-import torch.optim as optim
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+print("PyTorch Version: ",torch.__version__)
+print("Torchvision Version: ",torchvision.__version__)
+
+
+
 #CHECK FOR GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
