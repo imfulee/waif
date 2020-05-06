5d5839bea0b1ee69f35bbc2cdc9f5a8d306265d4
Maradonna <marco.jendryczko@gmx.net>
2020-05-06 13:03:35 +0200

    added missing import

--- train_classifier.py
+++ train_classifier.py
@@ -2,7 +2,9 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import torchvision.models as models
-
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
 #CHECK FOR GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
