46244bdbae5e5c910fc23d4850f42e792dfadfee
Maradonna <marco.jendryczko@gmx.net>
2020-05-06 18:08:10 +0200

    import and num classes

--- train_classifier.py
+++ train_classifier.py
@@ -69,6 +69,8 @@ test_ind = np.setdiff1d(indices, train_ind)
 
 train_set = torch.utils.data.Subset(train_set, train_ind)
 val_set = torch.utils.data.Subset(val_set, test_ind)
+print("train_set", train_set.classes)
+print("val_set", val_set.classes)
 image_datasets = {x: dset for dset, x in zip([train_set, val_set], ['train', 'val'])}
 
 # Create training and validation dataloaders
