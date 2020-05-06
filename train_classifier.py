77e83954e38b27f1aa1b2128344fa38ac2242d12
Maradonna <marco.jendryczko@gmx.net>
2020-05-06 13:38:52 +0200

    resize to 256

--- train_classifier.py
+++ train_classifier.py
@@ -11,7 +11,8 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #TODO: make everything for GPU
 # compose transformation
 transform = transforms.Compose(
-    [transforms.ToTensor(),
+    [transforms.Resize(256),
+     transforms.ToTensor(),
      transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
 
 #define sets
