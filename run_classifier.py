f214200a9a3a7c5943952398b413b71bc4089ecd
Maradonna <marco.jendryczko@gmx.net>
2020-06-11 17:54:38 +0200

    correct classes

--- run_classifier.py
+++ run_classifier.py
@@ -39,8 +39,7 @@ transform = transforms.Compose(
 #testloader = torch.utils.data.DataLoader(testset, batch_size=1,
 #                                         shuffle=False, num_workers=4)
 
-
-classes = ["Arabian", "African", "East Asian", "Mediterranean", "Scandinavian", "East European", "Central European", "South East Asian", "South American"]
+classes = ['African', 'Arabian', 'Central European', 'East Asian', 'East European', 'Mediterranean', 'Scandinavian', 'South American', 'South East Asian']
 
 # LOAD MODEL
 PATH = './FMGAN_net.pth'
